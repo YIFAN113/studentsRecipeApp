afec41a2899725b82ce8fccde4fc77eb119b32df
Yifan Gu <20095253@mail.wit.ie>
2024-02-03 21:03:36 +0800

    Rotes for get recipe and recipe details

---
+++ back-end/api/recipe/recipeData.js
@@ -0,0 +1,8 @@
+export const recipeData = {
+    page: 1,
+    total_pages: 1,
+    total_results: 3,
+    recipes: [{ id: "1", title: "noodle", ingredient: "noodle,egg"},
+    { id: "2", title: "fried chicken", ingredient: "chicken"},
+    { id: "3", title: "chips", ingredient: "potato"}]
+};
