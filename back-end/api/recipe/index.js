847a3d9bb18d8104977ab9a6639f3d7dbe0ba6cc
Yifan Gu <20095253@mail.wit.ie>
2024-02-04 22:00:35 +0800

    add Auth

--- back-end/api/recipe/index.js
+++ back-end/api/recipe/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Recipe from './recipeModel'
+import asyncHandler from 'express-async-handler';
 //import { v4 as uuidv4 } from 'uuid';
 const router = express.Router(); 
 
@@ -8,8 +9,8 @@ router.get('/', async (req, res) => {
     res.status(200).json(recipes);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
     const recipe = await Recipe(req.body).save();
     res.status(201).json(recipe);
-});
+}));
 export default router;
