4e25e2265d81329f6706e62fd7cfa0944e05d9c1
Yifan Gu <20095253@mail.wit.ie>
2024-02-17 22:02:55 +0800

    add bottomTabNavigator

--- appFrontEnd/App.js
+++ appFrontEnd/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-import Index from './src/pages/mapPage'
+//import Index from './src/pages/mapPage'
+import BottomTabNavigator from './src/navigator/bottom-tabs'
 export default class App extends Component {
   render() {
     return (
-      <Index/>
+      <BottomTabNavigator />
     )
   }
 }
