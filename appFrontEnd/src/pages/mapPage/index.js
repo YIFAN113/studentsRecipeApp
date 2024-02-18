7b5d9e14c0a239e6cf54f1469e3fd8a9f34a57bb
Yifan Gu <20095253@mail.wit.ie>
2024-02-18 23:02:23 +0800

    add highlight store location

--- appFrontEnd/src/pages/mapPage/index.js
+++ appFrontEnd/src/pages/mapPage/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
 const MapScreen = () => {
@@ -11,22 +11,39 @@ const MapScreen = () => {
     longitudeDelta: 0.0421,
   });
 
-  useEffect(() => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        setRegion({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          latitudeDelta: 0.005,
-          longitudeDelta: 0.005,
-        });
-      },
-      (error) => {
-        console.log(error.code, error.message);
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  }, []);
+const [shops, setShops] = useState([]);
+
+useEffect(() => {
+  Geolocation.getCurrentPosition(
+    (position) => {
+      const newRegion = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      };
+      setRegion(newRegion);
+      fetchShops(newRegion.latitude, newRegion.longitude);
+    },
+    (error) => {
+      console.log(error.code, error.message);
+    },
+    { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+  );
+}, []);
+
+const fetchShops = async (latitude, longitude) => {
+  const url = `http://10.0.2.2:8080/api/map/nearbyshops?latitude=${latitude}&longitude=${longitude}`;
+
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    setShops(json);
+  } catch (error) {
+    console.error('Error fetching nearby shops:', error);
+    Alert.alert('Error', 'Unable to fetch shops');
+  }
+};
 
   return (
     <View style={styles.container}>
@@ -34,8 +51,23 @@ const MapScreen = () => {
         style={styles.map}
         region={region}
         showsUserLocation={true}
-        followUserLocation={true}
-      />
+        followUserLocation={true}>
+
+   {shops.map((shop, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: shop.geometry.location.lat,
+              longitude: shop.geometry.location.lng,
+            }}
+            title={shop.name}
+            description={shop.description}
+          />
+        ))}
+        </MapView>
+
+        
+     
     </View>
   );
 };
